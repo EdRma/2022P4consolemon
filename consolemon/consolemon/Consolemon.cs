d53b2231c7ab122aefa91f79726af5696728dbf7
EdRma <34298@ma-web.nl>
2022-05-13 12:25:22 +0200

    classes

---
+++ consolemon/consolemon/Consolemon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolemon
+{
+    class Consolemon
+    {
+        int health = 108;
+        int attack = 130;
+        int defense = 95;
+        int energy = 150;
+        string name = "garchchomp";
+    }
+    
+}
---
+++ consolemon/consolemon/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolemon
+{
+    class Skill
+    {
+      int damage = 60;
+      int energyCost = 15;
+      int accuracy = 100;  
+      string name = "dragon breath";
+    }
+}
